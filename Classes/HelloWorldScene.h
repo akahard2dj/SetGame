7c9f05ab04fd7ccd7e38410ce48b79135ac88f81
akahard2dj <akahard2dj@naver.com>
2015-07-28 16:21:13 +0900

    Update HelloWorldScene.h

--- Classes/HelloWorldScene.h
+++ Classes/HelloWorldScene.h
@@ -13,32 +13,37 @@ class HelloWorld : public cocos2d::Layer
 public:
     // there's no 'id' in cpp, so we recommend returning the class instance pointer
     static cocos2d::Scene* createScene();
-    
+
     // Here's a difference. Method 'init' in cocos2d-x returns bool, instead of returning 'id' in cocos2d-iphone
     virtual bool init();
     
     // a selector callback
     void menuCloseCallback(cocos2d::Ref* pSender);
+	void gameRefreshCallback(cocos2d::Ref* pSender);
     
     // implement the "static create()" method manually
     CREATE_FUNC(HelloWorld);
-    
+
 public:
-    Layer* boardLayer;
-    Size winSize;
-    
-    std::vector<cocos2d::Sprite*> mGrid;
-    cocos2d::EventDispatcher* eventDispatcher;
-    CSetGame* pSetGame;
-    int* pBoardArray;
-    int answerCount;
-    int answerSheet[3];
-    
-    
+	Layer* boardLayer;
+	Size winSize;
+
+	std::vector<cocos2d::Sprite*> mGrid;
+	std::vector<int*> mGridIndex;
+	cocos2d::EventDispatcher* eventDispatcher;
+	CSetGame* pSetGame;
+	int* pBoardArray;
+	int answerCount;
+	int answerSheet[3];
+
+	
 public:
-    void drawGrid(int* array);
-    void addTileButtonEventListener();
-    
+	void drawGrid(int* array);
+	void refreshGrid(int* array);
+	void shuffleBoard();
+	void arrangeBoard(int* array);
+	void addTileButtonEventListener();
+
 };
 
 #endif // __HELLOWORLD_SCENE_H__
